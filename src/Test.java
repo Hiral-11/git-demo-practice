41e25ce3111f69a16ac2e187b69c816beb745ac3
Hiral-11 <146995776+Hiral-11@users.noreply.github.com>
2024-01-27 15:09:39 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My name is Hiral");
+        System.out.println("My name is Hiral, How are you?");
     }
 }
